a9ab110fce44301af270b1e53524ad224d911245
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-23 11:40:34 +0200

    Create 05_delete_a_node.cpp

---
+++ Problem Solving/Data Structures/Linked Lists/05_delete_a_node.cpp
@@ -0,0 +1,93 @@
+//https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    int layer = 0;
+    SinglyLinkedListNode *temp = head;
+
+    if(position == 0){
+        return head->next;
+    }
+
+    else{
+        while(layer<position-1){
+            ++layer;
+            temp = temp->next;
+        }
+
+        temp->next = temp->next->next;
+
+        return head;
+    }
+}
+
+int main()
