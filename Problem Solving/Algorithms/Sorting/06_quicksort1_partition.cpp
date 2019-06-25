73a4475361266d07d8e36ffc38f78362c14f823c
yang4978 <40636563+yang4978@users.noreply.github.com>
2019-06-25 10:23:54 +0200

    Create 06_quicksort1_partition.cpp

---
+++ Problem Solving/Algorithms/Sorting/06_quicksort1_partition.cpp
@@ -0,0 +1,94 @@
+//https://www.hackerrank.com/challenges/quicksort1/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the quickSort function below.
+vector<int> quickSort(vector<int> arr) {
+    int pivot = arr[0];
+    vector<int> result;
+    vector<int> right;
+    for(int i=1;i<arr.size();++i){
+        if(arr[i]<pivot){
+            result.push_back(arr[i]);
+        }
+        else{
+            right.push_back(arr[i]);
+        }
+    }
+    result.push_back(arr[0]);
+    for(auto i:right){
+        result.push_back(i);
+    }
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = quickSort(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
